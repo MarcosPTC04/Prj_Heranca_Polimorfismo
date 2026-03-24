6c1c21e13bedd0a3331e575b9dfaad2c37708c15
Marcos Antonio de Oliveira Filho <marcosantonio.ptc04@gmail.com>
2026-03-23 21:54:32 -0300

    Add files via upload

---
+++ src/ManterArquivo.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.prjexemploheranca;
+
+import java.io.File;
+
+/**
+ *
+ * @author Iftm
+ */
+public class ManterArquivo {
+    private final String NOME_ARQUIVO = "c: teste.txt";
+    public void escreverArquivo(Pessoa p){
+        File f = new File(NOME_ARQUIVO);
+    }
+    
+    public void lerArquivo(){
+        
+    }
+    
+    
+}
